3f7db6a6f7c144c16386007f35b80eb938a1492f
xylong <416319808@qq.com>
2018-09-23 22:44:37 +0800

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,11 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Http\Requests;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    public function create(){
+    public function create()
+    {
         return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        session()->flash('success', '👏，您将在这里开启一段新的旅程～');
+        return redirect()->route('users.show', [$user]);
     }
 }
